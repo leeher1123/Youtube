54eb35fa3d41d30b0179e1766f78baf07cc0a6e7
leeher1123 <leeher1123@naver.com>
2021-09-01 17:32:47 +0900

    Icon, Globalstyle, Router 의 추가

---
+++ src/views/shared/components/Button/Button.Styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const DefaultButton = styled.button`
+  background: transparent;
+  border: 1px solid transparent;
+  outline: 0;
+  padding: 0;
+  cursor: pointer;
+`;
