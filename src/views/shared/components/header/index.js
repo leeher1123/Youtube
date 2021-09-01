dbebdcc614c4fa8c6b41f2ea7c4829680e2553fa
leeher1123 <leeher1123@naver.com>
2021-09-01 17:33:33 +0900

    Header Logo UI 컴포넌트의 추가

---
+++ src/views/shared/components/header/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { IconLogo, IconMenuBar } from '../../../../icon';
+import SearchBox from './SearchBox';
+import IconMenus from './IconMenus';
+
+const Header = () => {
+  const a = 1;
+  return (
+    <Container>
+      <Logo>
+        <BarIcon><IconMenuBar /></BarIcon>
+        <LogoIcon><IconLogo /></LogoIcon>
+      </Logo>
+      <SearchBox />
+      <IconMenus />
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  padding: 0 16px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #212121;
+  height: 56px;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const BarIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  padding: 8px;
+
+  svg {
+    width: 24px;
+    height: 24px;
+    fill: #fff;
+  }
+`;
+
+const LogoIcon = styled.div`
+  width: 120px;
+  height: 56px;
+  padding: 18px 14px 18px 16px;
+  svg {
+    width: 90px;
+    height: 20px;
+    fill: #fff;
+  }
+`;
+
+export default Header;
