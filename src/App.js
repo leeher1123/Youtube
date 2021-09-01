1f6d29236f10d18cf6ea0ac39a3314b1ccfd38cf
leeher1123 <leeher1123@naver.com>
2021-09-01 13:30:55 +0900

    리액트 초기설정 및 eslint 추가

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const App = () => {
+  const a = 1;
+  return (
+    <Container>
+      App
+    </Container>
+  );
+};
+
+const Container = styled.div`
+
+`;
+
+export default App;
