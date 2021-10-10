e486e2ddb9d2ba57c1505a72d2ebbbb0e2a6c642
leeher1123 <leeher1123@naver.com>
2021-10-10 22:48:55 +0900

    count 수정

--- src/lib/count.js
+++ src/lib/count.js
@@ -5,9 +5,12 @@ export const count = (sum) => {
     return `${(sum / 1000).toFixed(1)}천`;
   }
   if (sum >= 10000 && sum < 100000) {
-    return `${(sum / 10000).toFixed(1)}만`;
+    return `${Math.floor(sum / 10000)}만`;
   }
-  if (sum >= 100000) {
-    return `${(sum / 10000)}만`;
+  if (sum >= 100000 && sum < 1000000) {
+    return `${Math.floor(sum / 10000)}만`;
+  }
+  if (sum >= 1000000) {
+    return `${Math.floor(sum / 10000)}만`;
   }
 };
