ff82c8db6dce4ac9913dddb4a73703fb7a3c59df
leeher1123 <leeher1123@naver.com>
2021-10-15 17:20:26 +0900

    watch 페이지 반응형 구현

--- src/views/watch/components/WatchVideo.js
+++ src/views/watch/components/WatchVideo.js
@@ -1,33 +1,52 @@
 import React from 'react';
 import styled from 'styled-components';
-import YouTube from 'react-youtube';
 import { useParams } from 'react-router-dom';
 
 import WatchContents from './WatchContents';
 
 const WatchVideo = ({ videos }) => {
   const { videoId } = useParams();
-  const opts = {
-    width: '948',
-    height: '535',
-    playerVars: {
-      autoplay: 1,
-    },
-  };
 
   if (!videos) return '...loading';
 
   return (
     <Container>
-      <YouTube videoId={videoId} opts={opts} />
+      <Video>
+        <Inner>
+          <iframe
+            width="100%"
+            height="100%"
+            src={`https://www.youtube.com/embed/${videoId}`}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        </Inner>
+      </Video>
       <WatchContents data={videos.items} />
     </Container>
   );
 };
 
 const Container = styled.div`
-  max-width: 1387px;
+  width: 100%;
   padding: 24px 24px 0;
+`;
+
+const Video = styled.div`
+  position: relative;
+  padding-bottom: 50%;
+  height: 0;
+  overflow: hidden;
+`;
+
+const Inner = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 `;
 
 export default WatchVideo;
